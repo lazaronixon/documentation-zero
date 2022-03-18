de8d3e0b1171e93e3db883fd1e0898a33749e257
Nixon <lazaronixon@hotmail.com>
2022-03-17 23:47:17 -0300

    create generator

--- lib/generators/documentation/readme/readme_generator.rb
+++ lib/generators/documentation/readme/readme_generator.rb
@@ -2,6 +2,19 @@ module Documentation
   module Generators
     class ReadmeGenerator < Rails::Generators::Base
       source_root File.expand_path("templates", __dir__)
+
+      def copy_files
+        template "README.md", ".documentation_api/README.md"
+      end
+
+      private
+        def application_name
+          if defined?(Rails) && Rails.application
+            Rails.application.class.name.split("::").first.underscore
+          else
+            "application"
+          end
+        end
     end
   end
 end
--- lib/generators/documentation/scaffold/scaffold_generator.rb
+++ lib/generators/documentation/scaffold/scaffold_generator.rb
@@ -3,6 +3,10 @@ module Documentation
     class ScaffoldGenerator < Rails::Generators::NamedBase
       argument :attributes, type: :array, default: [], banner: "field:type field:type"
       source_root File.expand_path("templates", __dir__)
+
+      def copy_files
+        template "resource.md", ".documentation_api/sections/#{plural_file_name}.md"
+      end
     end
   end
 end
