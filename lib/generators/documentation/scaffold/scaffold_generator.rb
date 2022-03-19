b5bd18257aba652506f385012b151e4d0a14358c
Nixon <lazaronixon@hotmail.com>
2022-03-19 20:06:52 -0300

    option timestamps

--- lib/generators/documentation/scaffold/scaffold_generator.rb
+++ lib/generators/documentation/scaffold/scaffold_generator.rb
@@ -2,6 +2,9 @@ module Documentation
   module Generators
     class ScaffoldGenerator < Rails::Generators::NamedBase
       argument :attributes, type: :array, default: [], banner: "field:type field:type"
+
+      class_option :timestamps, type: :boolean, default: true
+
       source_root File.expand_path("templates", __dir__)
 
       def copy_files
